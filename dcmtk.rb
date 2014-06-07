3db31e197ac92efa83e5952da6fe3be9d7e46eba
sormy <art.sormy@gmail.com>
2014-06-08 01:58:35 +0400

    Update dcmtk.rb
    main version for dcmtk is 3.6.1
    fixes for Offset patch for dcmtk

--- dcmtk.rb
+++ dcmtk.rb
@@ -2,10 +2,8 @@ require 'formula'
 
 class Dcmtk < Formula
   homepage 'http://dicom.offis.de/dcmtk.php.en'
-  head 'http://dicom.offis.de/download/dcmtk/snapshot/dcmtk-3.6.1_20131114.tar.gz'
-  url 'ftp://dicom.offis.de/pub/dicom/offis/software/dcmtk/dcmtk360/dcmtk-3.6.0.tar.gz'
-  sha1 '469e017cffc56f36e834aa19c8612111f964f757'
-  revision 1
+  url 'http://dicom.offis.de/download/dcmtk/snapshot/dcmtk-3.6.1_20131114.tar.gz'
+  sha1 'a6b5f8b1f4a78a3955e2084919f9b9e346f1c6de'
 
   option 'with-docs', 'Install development libraries/headers and HTML docs'
 
@@ -398,35 +396,36 @@ index 1a7f208..ee48743 100644
            T *newItem = new T( item );
 diff -ur dcmtk-3.6.0/dcmdata/include/dcmtk/dcmdata/dcobject.h dcmtk-3.6.0-new/dcmdata/include/dcmtk/dcmdata/dcobject.h
 --- dcmtk-3.6.0/dcmdata/include/dcmtk/dcmdata/dcobject.h	2010-10-29 14:57:17.000000000 +0400
-+++ dcmtk-3.6.0-new/dcmdata/include/dcmtk/dcmdata/dcobject.h	2014-06-08 00:19:00.000000000 +0400
-@@ -503,6 +503,8 @@
++++ dcmtk-3.6.0-new/dcmdata/include/dcmtk/dcmdata/dcobject.h	2014-06-08 01:53:53.000000000 +0400
+@@ -503,6 +503,9 @@
       */
      Uint32 getLengthField() const { return Length; }
  
-+    Uint32 getFileOffset() const { return fOffset; }
++    Uint32 getOffset() const { return Offset; }
++    void setOffset(Uint32 newOffset) { Offset = newOffset; }
 +
   protected:
  
      /** print line indentation, e.g. a couple of spaces for each nesting level.
-@@ -671,6 +673,9 @@
+@@ -671,6 +674,9 @@
      /// the length of this attribute as read from stream, may be undefined length
      Uint32 Length;
  
 +    /// orignal file offset to read value from source file
-+    Uint32 fOffset;
++    Uint32 Offset;
 +
      /// transfer state during read and write operations
      E_TransferState fTransferState;
  
 diff -ur dcmtk-3.6.0/dcmdata/libsrc/dcelem.cc dcmtk-3.6.0-new/dcmdata/libsrc/dcelem.cc
 --- dcmtk-3.6.0/dcmdata/libsrc/dcelem.cc	2010-11-05 12:34:14.000000000 +0300
-+++ dcmtk-3.6.0-new/dcmdata/libsrc/dcelem.cc	2014-06-08 00:27:33.000000000 +0400
++++ dcmtk-3.6.0-new/dcmdata/libsrc/dcelem.cc	2014-06-08 01:54:13.000000000 +0400
 @@ -1049,6 +1049,8 @@
              }
              /* if the transfer state is ERW_inWork and we are not supposed */
              /* to read this element's value later, read the value now */
 +            if (getTransferState() == ERW_inWork)
-+                fOffset = inStream.tell();
++                setOffset(inStream.tell());
              if (getTransferState() == ERW_inWork && !fLoadValue)
                  errorFlag = loadValue(&inStream);
              /* if the amount of transferred bytes equals the Length of this element */
@@ -435,7 +434,7 @@ diff -ur dcmtk-3.6.0/dcmdata/libsrc/dcelem.cc dcmtk-3.6.0-new/dcmdata/libsrc/dce
      /* value length in bytes = 0..max */
      out << " len=\"" << getLengthField() << "\"";
 +    /* offset in bytes from beginning of file */
-+    out << " offset=\"" << getFileOffset() << "\"";
++    out << " offset=\"" << getOffset() << "\"";
      /* tag name (if known and not suppressed) */
      if (!(flags & DCMTypes::XF_omitDataElementName))
          out << " name=\"" << OFStandard::convertToMarkupString(getTagName(), xmlString) << "\"";
