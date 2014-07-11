3a0ad51592d508e3e9a960b568d72366cf795721
Artem Butusov <sormy@Artems-Mac-Pro.local>
2014-07-11 18:31:59 -0400

    wkhtmltox: added 0.11.1 release and 0.11.2 fresh dev versions

--- wkhtmltox.rb
+++ wkhtmltox.rb
@@ -2,13 +2,27 @@ require "formula"
 
 class Wkhtmltox < Formula
   homepage "http://wkhtmltopdf.org"
-  url "http://downloads.sourceforge.net/project/wkhtmltopdf/0.12.1-dev/wkhtmltox-0.12.1-7fac78c_osx-10.9-cocoa-x86-64.tar.xz"
-  sha1 "7210f18f4023e0c0b8e6588d25d80d2e7e2c1591"
-  version "0.12.1-7fac78c"
+
+  url "http://downloads.sourceforge.net/project/wkhtmltopdf/0.12.1/wkhtmltox-0.12.1_osx-cocoa-x86-64.pkg"
+  version "0.12.1"
+  sha1 "f9f4b7e00d811cbbc51f21879653e77dc539ba68"
+
+  devel do
+    url "http://downloads.sourceforge.net/project/wkhtmltopdf/0.12.2-dev/wkhtmltox-0.12.2-6a13a51_osx-cocoa-x86-64.pkg"
+    sha1 "82d9599c0077a6b3b33c4c0c01c435b2a19a790e"
+    version "0.12.2-6a13a51"
+  end
+
+  depends_on "xz" => :build
 
   def install
+    system "cat Payload | gunzip -dc | cpio -i"
+    system "xz -d usr/local/share/wkhtmltox-installer/app.tar.xz"
+    system "tar -xf usr/local/share/wkhtmltox-installer/app.tar"
+
     bin.install Dir['bin/*']
     lib.install Dir['lib/*']
     include.install Dir['include/*']
+    man1.install Dir['share/man/man1/*']
   end
 end
