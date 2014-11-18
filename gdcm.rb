4cb69391d0ac37b5c1661f9ff094b31d2b60f743
Artem Butusov <art.sormy@gmail.com>
2014-11-18 00:43:42 -0500

    updated gdcm to 2.4.4 version

--- gdcm.rb
+++ gdcm.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Gdcm < Formula
   homepage 'http://gdcm.sourceforge.net'
-  url 'http://downloads.sourceforge.net/project/gdcm/gdcm%202.x/GDCM%202.4.2/gdcm-2.4.2.tar.bz2'
-  sha1 '57251e6bdec2654df20039046c15f041c2a71035'
+  url 'http://downloads.sourceforge.net/project/gdcm/gdcm%202.x/GDCM%202.4.4/gdcm-2.4.4.tar.bz2'
+  sha1 '4b77a857cf8432da72140a5e7d20f78c091ee019'
 
   depends_on 'cmake' => :build
 
