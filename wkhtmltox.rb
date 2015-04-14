f3b0c4497d296f023d598f0e73718d180ca02e13
Artem Butusov <art.sormy@gmail.com>
2015-04-14 17:24:12 -0400

    wkhtmltox v0.12.2.1

--- wkhtmltox.rb
+++ wkhtmltox.rb
@@ -3,15 +3,9 @@ require "formula"
 class Wkhtmltox < Formula
   homepage "http://wkhtmltopdf.org"
 
-  url "http://downloads.sourceforge.net/project/wkhtmltopdf/0.12.1/wkhtmltox-0.12.1_osx-cocoa-x86-64.pkg"
-  version "0.12.1"
-  sha1 "f9f4b7e00d811cbbc51f21879653e77dc539ba68"
-
-  devel do
-    url "http://downloads.sourceforge.net/project/wkhtmltopdf/0.12.2-dev/wkhtmltox-0.12.2-6a13a51_osx-cocoa-x86-64.pkg"
-    sha1 "82d9599c0077a6b3b33c4c0c01c435b2a19a790e"
-    version "0.12.2-6a13a51"
-  end
+  url "http://downloads.sourceforge.net/project/wkhtmltopdf/0.12.2.1/wkhtmltox-0.12.2.1_osx-cocoa-x86-64.pkg"
+  version "0.12.2.1"
+  sha1 "066019461b5e2e0ec88dacd009494fb3a8afad07"
 
   depends_on "xz" => :build
 
