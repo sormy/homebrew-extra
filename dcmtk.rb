48c6bff5cd4217209de026b9128427f186343338
Artem Butusov <artem.butusov@unvsoft.com>
2015-09-29 23:03:45 -0400

    Fix dcmtk to satisfy vanilla formula

--- dcmtk.rb
+++ dcmtk.rb
@@ -1,20 +1,24 @@
-require 'formula'
-
 class Dcmtk < Formula
-  homepage 'http://dicom.offis.de/dcmtk.php.en'
-  url 'http://dicom.offis.de/download/dcmtk/snapshot/dcmtk-3.6.1_20150629.tar.gz'
-  sha256 '6af8a4683a8f4995cefbad00e727fd760e0e5f535d7c4ad622ce280a701888e2'
-  version '3.6.1_20150629'
+  desc "OFFIS DICOM toolkit command-line utilities"
+  homepage "http://dicom.offis.de/dcmtk.php.en"
   
-  head 'http://git.dcmtk.org/dcmtk.git'
+  # Current snapshot used for stable now.
+  url "http://dicom.offis.de/download/dcmtk/snapshot/dcmtk-3.6.1_20150629.tar.gz"
+  sha256 "6af8a4683a8f4995cefbad00e727fd760e0e5f535d7c4ad622ce280a701888e2"
+  version "3.6.1-20150629"
+  
+  head "http://git.dcmtk.org/dcmtk.git"
 
-  option 'with-docs', 'Install development libraries/headers and HTML docs'
-  option 'with-openssl', 'Configure DCMTK with support for OpenSSL'
-  
-  depends_on 'cmake' => :build
-  depends_on 'libpng'
-  depends_on 'libtiff'
-  depends_on 'doxygen' if build.with? "docs"
+  option "with-docs", "Install development libraries/headers and HTML docs"
+  option "with-openssl", "Configure DCMTK with support for OpenSSL"
+  option "with-libiconv", "Build with brewed libiconv. Dcmtk and system libiconv can have problems with utf8."
+
+  depends_on "cmake" => :build
+  depends_on "doxygen" => :build if build.with? "docs"
+  depends_on "libpng"
+  depends_on "libtiff"
+  depends_on "openssl"
+  depends_on "homebrew/dupes/libiconv" => :optional
 
   patch do
     url "file://" + File.dirname(__FILE__) + "/patches/dcmtk-3.6.1-dcm2xml-offsets.patch"
@@ -30,14 +34,21 @@ class Dcmtk < Formula
     ENV.m64 if MacOS.prefer_64_bit?
 
     args = std_cmake_args
-    args << '-DDCMTK_WITH_DOXYGEN=YES' if build.with? "docs"
-    args << '-DDCMTK_WITH_OPENSSL=YES' if build.with? "openssl"
-    args << '..'
+    args << "-DDCMTK_WITH_DOXYGEN=YES" if build.with? "docs"
+    args << "-DDCMTK_WITH_OPENSSL=YES" if build.with? "openssl"
+    args << "-DDCMTK_WITH_ICONV=YES -DLIBICONV_DIR=#{Formula["libiconv"].opt_prefix}" if build.with? "libiconv"
+    args << ".."
 
-    mkdir 'build' do
-      system 'cmake', *args
-      system 'make DOXYGEN' if build.with? "docs"
-      system 'make install'
+    mkdir "build" do
+      system "cmake", *args
+      system "make", "DOXYGEN" if build.with? "docs"
+      system "make", "install"
     end
+  end
+
+  test do
+    system bin/"pdf2dcm", "--verbose",
+           test_fixtures("test.pdf"), testpath/"out.dcm"
+    system bin/"dcmftest", testpath/"out.dcm"
   end
 end
